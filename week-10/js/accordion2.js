c1575110d2b36d64cc903f355b903969944615df
palexey2006 <palexey2006@gmail.com>
2026-03-20 10:15:51 -0400

    done lesson 10 job

--- week-10/js/accordion2.js
+++ week-10/js/accordion2.js
@@ -1,15 +1,23 @@
 // STEP 6: Nab all the SUMMARY elements
-
+const details = document.querySelectorAll('details');
 
 // STEP 7: Loop through the element array, and add an event listener for the toggle event
+details.forEach(detail => {
+    // STEP 8a: Check to see whether the DETAILS event that was toggled has the "open" attribute
+    detail.addEventListener('toggle', (e) => {
+        if(event.target.open){
+            details.forEach(detail => {
 
+                // STEP 8b: The browser just added the "open" attribute, so this DETAILS element was previously closed
+                // STEP 8c: If this DETAILS element is NOT the one that was just clicked, close it by removing the "open" attribute
+                if(detail != event.target) {
+                    detail.removeAttribute("open");
+                }
 
-// STEP 8a: Check to see whether the DETAILS event that was toggled has the "open" attribute
-
-// STEP 8b: The browser just added the "open" attribute, so this DETAILS element was previously closed
-
-// STEP 8c: If this DETAILS element is NOT the one that was just clicked, close it by removing the "open" attribute
-
+            })
+        }
+    })
+});
 
 
 
